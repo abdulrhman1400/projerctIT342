60f5a1b6ad6e1706e34d040e9c1596b5909aa21c
عبدالرحمن <nsrea_95@hotmail.com>
2019-03-04 17:04:16 +0300

    added navigation

---
+++ IT342/App/navigator/Navigator.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+
+import Home from '../screens/Home';
+
+const AppNavigator = createStackNavigator({
+    Home: { screen: Home }
+  });
+  
+  export default createAppContainer(AppNavigator);
